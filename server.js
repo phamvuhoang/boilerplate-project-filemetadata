3e141bd81ffaf98e6ab0f57bd5b4aff1bd978548
hoangpv <hoangpv@LAP001161.local>
2019-01-13 10:28:11 +0700

    complete challenge

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var upload = multer({dest: 'assets/'});
 
 var app = express();
 
@@ -16,6 +18,20 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+// I can submit a form that includes a file upload.
+// The from file input field has the "name" attribute set to "upfile". We rely on this in testing.
+// When I submit something, I will receive the file name and size in bytes within the JSON response
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
+  if (req.file) {
+    res.json({
+      filename: req.file.originalname,
+      filesize: req.file.size
+    });
+  }
+  
+  next('No file selected');
 });
 
 app.listen(process.env.PORT || 3000, function () {
